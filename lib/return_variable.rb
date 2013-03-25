4d71ab18856a2a5c3383fb49674c8a282b03a615
Nick Markwell <nick@duckinator.net>
2013-03-25 14:42:59 -0400

    Misc cleanup.

--- lib/return_variable.rb
+++ lib/return_variable.rb
@@ -16,15 +16,14 @@ class ReturnVariable
     @code.gsub!(/return\s*=\s*/, '____return = ')
     @code.gsub!(/return\s*$/,    '____return ||= nil')
 
-    @code.gsub!('__END__', '') # HACK
+    @code.gsub!('__END__', '') # Hack to allow for using it via `require`.
   end
 
   def run_modified
     # $DONTMODIFY is to tell it to not modify the code.
     # This avoids recursively calling ReturnVariable.
     code = "$DONTMODIFY = true; " + @code
-puts code
-puts '-----------------'
+
     eval code
     exit
   end
